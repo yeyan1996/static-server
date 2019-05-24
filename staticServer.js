1b370721e980faf64af7af4e1fe5acfb3d88b41c
yeyan1996 <1366792281@qq.com>
2019-05-24 18:10:13 +0800

    feat：将原本逻辑转为 class

--- staticServer.js
+++ staticServer.js
@@ -1,5 +1,18 @@
 "use strict";
 // nodejs 搭建静态服务器
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,60 +22,120 @@ var http_1 = __importDefault(require("http"));
 var path_1 = __importDefault(require("path"));
 var mime_1 = __importDefault(require("mime"));
 var zlib_1 = __importDefault(require("zlib"));
+var crypto_1 = __importDefault(require("crypto"));
 var PORT = 3001;
 var GzipRE = /\bgzip\b/;
 var DeflateRE = /\bdeflate\b/;
-var server = http_1.default.createServer();
-// 监听 request 事件
-server.on('request', function (req, res) {
-    // 对 ico 资源文件的处理
-    console.log(req.url);
-    try {
-        var readStream = void 0;
-        var assetPath = void 0;
-        // 检查是否有该文件，没有会报错并被捕获
-        var stat = fs_1.default.statSync(path_1.default.join(__dirname, req.url));
-        // 缓存
-        var ifModifiedSince = req.headers['if-modified-since'];
-        if (ifModifiedSince) {
-            var lastModified = stat.ctime.toUTCString();
-            if (lastModified === ifModifiedSince) {
-                res.statusCode = 304;
-                res.end();
+var MyServer = /** @class */ (function (_super) {
+    __extends(MyServer, _super);
+    function MyServer() {
+        var _this = _super.call(this) || this;
+        _this.stat = null;
+        _this.readStream = null;
+        _this.assetPath = "";
+        _this._init();
+        return _this;
+    }
+    MyServer.prototype._init = function () {
+        var _this = this;
+        // 监听 request 事件
+        this.on('request', function (req, res) {
+            try {
+                _this.handleRequest(req, res);
+                if (!_this.checkLastModified(req, res)) {
+                    _this.sendFile(req, res);
+                    return;
+                }
+                _this.handleEncoding(req, res);
+                if (!_this.checkEtag(req, res)) {
+                    _this.sendFile(req, res);
+                    return;
+                }
+                _this.sendFile(req, res);
             }
-            else {
-                res.setHeader('last-modified', lastModified);
+            catch (e) {
+                console.error(e);
+                _this.handleNotFound(req, res);
             }
-        }
-        // __dirname 为当前执行文件所在的完整目录名（绝对路径）eg. "C:\Users\007\Desktop\static-server"
-        // process.cwd 为当前执行 node 命令的文件目录名（绝对路径）eg. "C:\Users\007\Desktop\static-server\nodejs.js"
-        // __filename 为当前执行文件的完整目录名 （绝对路径） eg. "C:\Users\007\Desktop\static-server\nodejs.js"
+        });
+        this.listen(PORT, function () {
+            console.log("nodejs \u670D\u52A1\u5668\u6B63\u5728\u8FD0\u884C,\u7AEF\u53E3:" + PORT);
+        });
+        this.on('error', function (e) {
+            console.log(e);
+        });
+    };
+    // 根据请求寻找目标文件
+    MyServer.prototype.handleRequest = function (req, res) {
+        console.log(req.url);
+        if (!req.url)
+            throw new Error('找不到 url');
+        this.stat = fs_1.default.statSync(path_1.default.join(__dirname, req.url));
         // 寻找静态资源
-        assetPath = req.url === '/' ? path_1.default.join(__dirname, '/nodejs.html') : path_1.default.join(__dirname, req.url);
-        readStream = fs_1.default.createReadStream(assetPath);
-        res.setHeader("content-type", mime_1.default.getType(assetPath));
-        // 压缩
+        this.assetPath = req.url === '/' ? path_1.default.join(__dirname, '/nodejs.html') : path_1.default.join(__dirname, req.url);
+        this.readStream = fs_1.default.createReadStream(this.assetPath);
+    };
+    // 压缩相关
+    MyServer.prototype.handleEncoding = function (req, res) {
         var acceptEncoding = req.headers["accept-encoding"];
+        if (!acceptEncoding || !this.readStream)
+            throw new Error('找不到 url');
         if (acceptEncoding.match(GzipRE)) {
             res.setHeader("Content-Encoding", "gzip");
-            readStream.pipe(zlib_1.default.createGzip()).pipe(res);
+            this.readStream = this.readStream.pipe(zlib_1.default.createGzip());
         }
         else if (acceptEncoding.match(DeflateRE)) {
             res.setHeader("Content-Encoding", "deflate");
-            readStream.pipe(zlib_1.default.createDeflate()).pipe(res);
+            this.readStream = this.readStream.pipe(zlib_1.default.createDeflate());
         }
-        else {
-            readStream.pipe(res);
+    };
+    MyServer.prototype.sendFile = function (req, res) {
+        if (!req.url)
+            throw new Error('找不到 url');
+        // __dirname 为当前执行文件所在的完整目录名（绝对路径）eg. "C:\Users\007\Desktop\static-server"
+        // process.cwd 为当前执行 node 命令的文件目录名（绝对路径）eg. "C:\Users\007\Desktop\static-server\nodejs.js"
+        // __filename 为当前执行文件的完整目录名 （绝对路径） eg. "C:\Users\007\Desktop\static-server\nodejs.js"
+        if (mime_1.default.getType(this.assetPath)) {
+            res.setHeader("content-type", mime_1.default.getType(this.assetPath) + ';charset=utf-8');
         }
-    }
-    catch (e) {
+        // 走到这步时 readStream 一定存在，不存在会直接走 handleNotFound
+        this.readStream.pipe(res);
+    };
+    // 协商缓存（文件最后修改时间）
+    MyServer.prototype.checkLastModified = function (req, res) {
+        if (!this.stat)
+            throw new Error('找不到文件');
+        var lastModified = this.stat.ctime.toUTCString();
+        var ifModifiedSince = req.headers['if-modified-since'];
+        res.setHeader('last-modified', lastModified);
+        if (lastModified === ifModifiedSince) {
+            res.statusCode = 304;
+            res.end();
+            return false;
+        }
+        return true;
+    };
+    // 协商缓存（etag）
+    MyServer.prototype.checkEtag = function (req, res) {
+        if (!this.readStream)
+            throw new Error('找不到文件');
+        var md5 = crypto_1.default.createHash('md5');
+        // 通过最后修改时间和文件大小计算出 etag
+        var etag = md5.update(this.stat.ctime.toUTCString() + this.stat.size).digest('hex');
+        var ifNoneMatch = req.headers['if-none-match'];
+        res.setHeader('Etag', etag);
+        if (ifNoneMatch === etag) {
+            res.statusCode = 304;
+            res.end();
+            return false;
+        }
+        return true;
+    };
+    MyServer.prototype.handleNotFound = function (req, res) {
         res.statusCode = 404;
-        res.end('not found');
-    }
-});
-server.listen(PORT, function () {
-    console.log('nodejs 服务器正在运行');
-});
-server.on('error', function (e) {
-    console.log(e);
-});
+        res.end();
+    };
+    return MyServer;
+}(http_1.default.Server));
+new MyServer();
+//# sourceMappingURL=staticServer.js.map
--- staticServer.ts
+++ staticServer.ts
@@ -5,71 +5,128 @@ import http from 'http';
 import path from 'path';
 import mime from 'mime';
 import zlib from 'zlib';
-
+import crypto from 'crypto'
+import stream from 'stream'
 
 const PORT: number = 3001
 const GzipRE: RegExp = /\bgzip\b/
 const DeflateRE: RegExp = /\bdeflate\b/
 
-const server = http.createServer()
 
-// 监听 request 事件
-server.on('request', (req, res) => {
-    // 对 ico 资源文件的处理
-    console.log(req.url)
+class MyServer extends http.Server {
 
-    try {
-        let readStream: fs.ReadStream;
-        let assetPath: string;
-        // 检查是否有该文件，没有会报错并被捕获
-        let stat: fs.Stats = fs.statSync(path.join(__dirname, req.url))
+    private stat: fs.Stats | null;
+    private readStream: stream.Stream | null
+    private assetPath: string
 
+    constructor() {
+        super()
+        this.stat = null
+        this.readStream = null
+        this.assetPath = ""
+        this._init()
+    }
 
-        // 缓存
-        let ifModifiedSince: string | null = req.headers['if-modified-since']
-        if (ifModifiedSince) {
-            let lastModified: string = stat.ctime.toUTCString()
-            if (lastModified === ifModifiedSince) {
-                res.statusCode = 304
-                res.end()
-            } else {
-                res.setHeader('last-modified', lastModified)
+    private _init() {
+        // 监听 request 事件
+        this.on('request', (req, res) => {
+            try {
+                this.handleRequest(req, res)
+                if (!this.checkLastModified(req, res)){
+                    this.sendFile(req, res)
+                    return
+                }
+                this.handleEncoding(req, res)
+                if (!this.checkEtag(req, res)) {
+                    this.sendFile(req, res)
+                    return
+                }
+                this.sendFile(req, res)
+            } catch (e) {
+                console.error(e)
+                this.handleNotFound(req, res)
             }
+        })
+        this.listen(PORT, () => {
+            console.log(`nodejs 服务器正在运行,端口:${PORT}`)
+        })
+        this.on('error', e => {
+            console.log(e)
+        })
+    }
+
+    // 根据请求寻找目标文件
+    handleRequest(req: http.IncomingMessage, res: http.ServerResponse) {
+        console.log(req.url)
+        if (!req.url) throw new Error('找不到 url')
+        this.stat = fs.statSync(path.join(__dirname, req.url))
+        // 寻找静态资源
+        this.assetPath = req.url === '/' ? path.join(__dirname, '/nodejs.html') : path.join(__dirname, req.url)
+        this.readStream = fs.createReadStream(this.assetPath)
+    }
+
+    // 压缩相关
+    handleEncoding(req: http.IncomingMessage, res: http.ServerResponse) {
+        let acceptEncoding: string | undefined = req.headers["accept-encoding"] as string | undefined
+        if (!acceptEncoding || !this.readStream) throw new Error('找不到 url')
+
+        if (acceptEncoding.match(GzipRE)) {
+            res.setHeader("Content-Encoding", "gzip");
+            this.readStream = this.readStream.pipe(zlib.createGzip())
+        } else if (acceptEncoding.match(DeflateRE)) {
+            res.setHeader("Content-Encoding", "deflate");
+            this.readStream = this.readStream.pipe(zlib.createDeflate())
         }
+    }
 
-
+    sendFile(req: http.IncomingMessage, res: http.ServerResponse): void {
+        if (!req.url) throw new Error('找不到 url')
         // __dirname 为当前执行文件所在的完整目录名（绝对路径）eg. "C:\Users\007\Desktop\static-server"
         // process.cwd 为当前执行 node 命令的文件目录名（绝对路径）eg. "C:\Users\007\Desktop\static-server\nodejs.js"
         // __filename 为当前执行文件的完整目录名 （绝对路径） eg. "C:\Users\007\Desktop\static-server\nodejs.js"
 
-        // 寻找静态资源
-        assetPath = req.url === '/' ? path.join(__dirname, '/nodejs.html') : path.join(__dirname, req.url)
-        readStream = fs.createReadStream(assetPath)
-        res.setHeader("content-type", mime.getType(assetPath))
-
-
-        // 压缩
-        let acceptEncoding: string = req.headers["accept-encoding"]
-        if (acceptEncoding.match(GzipRE)) {
-            res.setHeader("Content-Encoding", "gzip");
-            readStream.pipe(zlib.createGzip()).pipe(res)
-        } else if (acceptEncoding.match(DeflateRE)) {
-            res.setHeader("Content-Encoding", "deflate");
-            readStream.pipe(zlib.createDeflate()).pipe(res)
-        } else {
-            readStream.pipe(res)
+        if (mime.getType(this.assetPath)) {
+            res.setHeader("content-type", mime.getType(this.assetPath)! + ';charset=utf-8')
         }
-
-    } catch (e) {
-        res.statusCode = 404
-        res.end('not found')
+        // 走到这步时 readStream 一定存在，不存在会直接走 handleNotFound
+        this.readStream!.pipe(res)
     }
 
-})
+    // 协商缓存（文件最后修改时间）
+    checkLastModified(req: http.IncomingMessage, res: http.ServerResponse): boolean {
+        if (!this.stat) throw new Error('找不到文件')
+        let lastModified: string = this.stat.ctime.toUTCString()
+        let ifModifiedSince: string | undefined = req.headers['if-modified-since']
+        res.setHeader('last-modified', lastModified)
+        if (lastModified === ifModifiedSince) {
+            res.statusCode = 304
+            res.end()
+            return false
+        }
+        return true
+    }
 
-server.listen(PORT, () => {
-    console.log('nodejs 服务器正在运行')
-})
-server.on('error', e => {
-    console.log(e)
-})
+    // 协商缓存（etag）
+    checkEtag(req: http.IncomingMessage, res: http.ServerResponse): boolean {
+        if (!this.readStream) throw new Error('找不到文件')
+        let md5: crypto.Hash = crypto.createHash('md5')
+        // 通过最后修改时间和文件大小计算出 etag
+        let etag: string = md5.update(this.stat!.ctime.toUTCString() + this.stat!.size).digest('hex');
+        let ifNoneMatch: string | undefined = req.headers['if-none-match']
+        res.setHeader('Etag', etag)
+        if (ifNoneMatch === etag) {
+            res.statusCode = 304
+            res.end()
+            return false
+        }
+        return true
+    }
+
+    handleNotFound(req: http.IncomingMessage, res: http.ServerResponse): void {
+        res.statusCode = 404
+        res.end()
+    }
+}
+
+new MyServer()
+
